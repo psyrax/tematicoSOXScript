81f67183f041d6cd0ddc5306223366d1f119dca8
Psyrax <psyrax@sclbits.com>
2017-02-22 18:38:27 -0600

    iterative

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const exec = require('child_process').exec;
 const originalBack = __dirname + '/back.mp3';
 const lowerBack = __dirname + '/lowerBack.mp3';
-const audioTrack = __dirname + '/1.flac';
-const audioPadded = __dirname + '/1.mp3';
-const mixedTrack = __dirname + '/1mixed.mp3';
 const mp3Duration = require('mp3-duration');
 const spawn = require('child_process').spawn;
+const FileHound = require('filehound');
+const path = require('path');
 
-function lowerBackground(){
+function lowerBackground(audioTrack, audioPadded, mixedTrack){
 	console.log('Background lower');
 	var lowerTask = spawn('sox', [
 		'-V',
@@ -20,24 +19,24 @@ function lowerBackground(){
 	]);
 
 	lowerTask.stdout.on('data', (data) => {
-	  console.log(`${data}`);
+		console.log(`${data}`);
 	});
 
 	lowerTask.stderr.on('data', (data) => {
-	  console.log(`${data}`);
+		console.log(`${data}`);
 	});
 
 	lowerTask.on('close', (code) => {
-	  if ( code == 0 ){
-	  	console.log('Background lower complete');
-	  	padAudio();
-	  } else {
+		if ( code == 0 ){
+	  		console.log('Background lower complete');
+	  		padAudio(audioTrack, audioPadded, mixedTrack);
+		} else {
 		  	console.log('error', code);
-		  }
+		}
 	});
 };
 
-function padAudio(){
+function padAudio(audioTrack, audioPadded, mixedTrack){
 	console.log('Padding audio');
 	var padAudioTask = spawn('sox', [
 		'-V',
@@ -49,24 +48,24 @@ function padAudio(){
 		'3'
 	]);
 	padAudioTask.stdout.on('data', (data) => {
-	  console.log(`${data}`);
+		console.log(`${data}`);
 	});
 
 	padAudioTask.stderr.on('data', (data) => {
-	  console.log(`${data}`);
+		console.log(`${data}`);
 	});
 
 	padAudioTask.on('close', (code) => {
-	  if ( code == 0 ){
-	  	console.log('Pad complete');
-	  	mixTracks();
-	  } else {
-		console.log('error', code);
+		if ( code == 0 ){
+	  		console.log('Pad complete');
+	  		mixTracks(audioTrack, audioPadded, mixedTrack);
+		} else {
+			console.log('error', code);
 		}
 	});
 }
 
-function mixTracks(){
+function mixTracks(audioTrack, audioPadded, mixedTrack){
 	console.log('Starting Mix');
 	mp3Duration(audioPadded, function (err, duration) {
 		var mixTracksTask = spawn('sox', [
@@ -82,49 +81,38 @@ function mixTracks(){
 			duration
 		]);
 		mixTracksTask.stdout.on('data', (data) => {
-		  console.log(`${data}`);
+			console.log(`${data}`);
 		});
 
 		mixTracksTask.stderr.on('data', (data) => {
-		  console.log(`${data}`);
+			console.log(`${data}`);
 		});
 
 		mixTracksTask.on('close', (code) => {
-		  if ( code == 0 ){
-		  	console.log('Mixing complete');
-		  } else {
-		  	console.log('error', code);
-		  }
+			if ( code == 0 ){
+				console.log('Mixing complete');
+			} else {
+				console.log('error', code);
+			}
 		});
 	});
 }
 
-lowerBackground();
+function startTasks(audioTrack, audioPadded, mixedTrack){
+	lowerBackground(audioTrack, audioPadded, mixedTrack);
+};
 
-/*console.log('Lowering Background');
-exec('sox -V --rate 44100 -v 0.1 "' + originalBack + '" "' + lowerBack +'" repeat 5', (error, stdout, stderr) => {
-	if (error) {
-		console.error(`Lowering error: ${error}`);
-		return;
-	}
-	console.log(stderr);
-	console.log('Transcoding audio track');
-  	exec('sox -V "' + audioTrack + '" -r 44100 "' + audioPadded + '" pad 3 3' , (error, stdout, stderr) => {
-  		if (error) {
-    		console.error(`Transcoding error: ${error}`);
-    		return;
-  		}
-  		console.log(stderr);
-  		console.log('Getting duration');
-  		mp3Duration(audioPadded, function (err, duration) {
-			if (err) return console.log(err.message);
-  			exec('sox -V -m  -v 1 "' + lowerBack + '" -v 1 "' + audioPadded + '" "'+ mixedTrack +'" fade 2.5 '+ duration , (error, stdout, stderr) => {
-  				if (error) {
-    				console.error(`Mixing error: ${error}`);
-    				return;
-  				}
-  				console.log(stderr);
-			});
-		});
-	});
-});*/
+files = FileHound.create()
+  .paths(process.argv[2])
+  .ext('flac')
+  .find();
+
+files.then(function(found){
+	console.log(found);
+	found.forEach(function(file){
+		var audioTrack = file;
+		var audioPadded =  __dirname + '/tmp/' + path.basename(file, '.flac') + '.mp3';
+		var mixedTrack = __dirname + '/mixed/' + path.basename(file, '.flac') + '.mp3';
+		startTasks(audioTrack, audioPadded, mixedTrack);
+	})
+});
